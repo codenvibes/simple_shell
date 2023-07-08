f0c75c55fbc1096e6238e3a7ee953df76816b1b6
codenvibes <terrymk1999@gmail.com>
2023-07-08 13:27:34 +0300

    Create header file conatining 'display_prompt' function declaration

---
+++ prompt.h
@@ -0,0 +1,16 @@
+/*
+ * File: prompt.h
+ * Auth: Terrence M.K
+ *	 Tamara Lumumba
+ */
+
+#ifndef PROMPT_H
+#define PROMPT_H
+
+/**
+ * display_prompt - Displays the prompt for the user to enter a command
+ */
+void display_prompt(void);
+
+#endif /* PROMPT_H */
+
