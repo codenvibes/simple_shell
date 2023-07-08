b56abe0fd969b651ede1dc6d193c17ddd0aa8c6f
codenvibes <terrymk1999@gmail.com>
2023-07-08 13:29:07 +0300

    Create 'display_prompt' function definition

---
+++ prompt.c
@@ -0,0 +1,17 @@
+/*
+ * File: prompt.c
+ * Auth: Terrence M.K
+ *	 Tamara Lumumba
+ */
+
+#include <stdio.h>
+#include "prompt.h"
+
+/**
+ * display_prompt - Displays the prompt for the user to enter a command
+ */
+void display_prompt(void)
+{
+	printf("#cisfun$ ");
+}
+
