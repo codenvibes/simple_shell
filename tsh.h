ee1492353cd2a0466b5b7b3fbac9bb392f42d9fd
codenvibes <terrymk1999@gmail.com>
2023-07-24 21:56:00 +0300

    Create main header file

---
+++ tsh.h
@@ -0,0 +1,22 @@
+#ifndef TSH_H
+#define TSH_H
+
+/* Function Declarations for builtin shell commands */
+int tsh_cd(char **args);
+int tsh_help(char **args);
+int tsh_exit(char **args);
+
+/* Function Declarations for shell utility functions */
+int tsh_num_builtins(void);
+char *tsh_read_line(void);
+char **tsh_split_line(char *line);
+int tsh_execute(char **args);
+
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#endif /* TSH_H */
